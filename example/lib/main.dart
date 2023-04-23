89e9ae708f7463f413fc3bb945e4b81d023c41c0
mahmoud <eslamimahmoud98@gmail.com>
2023-04-23 12:53:11 +0330

    feat: refactor old logic and use renderbox

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:bottom_animation/bottom_animation.dart';
-import 'package:ionicons/ionicons.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,44 +10,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var items = <BottomNavItem>[
-    BottomNavItem(
-      title: 'logo',
-      widget: Icon(
-        Ionicons.logo_alipay,
-        color: Colors.white,
-      ),
-    ),
-    BottomNavItem(
-      title: 'profile',
-      widget: Icon(
-        Ionicons.at_circle_outline,
-        color: Colors.white,
-      ),
-    ),
-    BottomNavItem(
-      title: 'profile',
-      widget: Icon(
-        Ionicons.at_circle_outline,
-        color: Colors.white,
-      ),
-    ),
-    BottomNavItem(
-      title: 'profile',
-      widget: Icon(
-        Ionicons.at_circle_outline,
-        color: Colors.white,
-      ),
-    ),
-    BottomNavItem(
-      title: 'profile',
-      widget: Icon(
-        Ionicons.at_circle_outline,
-        color: Colors.white,
-      ),
-    ),
-  ];
-
   var cIndex;
   @override
   void initState() {
@@ -67,30 +27,154 @@ class _MyAppState extends State<MyApp> {
       ),
       home: Scaffold(
         backgroundColor: Colors.white,
-        bottomNavigationBar: BottomAnimation(
-          isFloating: true,
-          selectedIndex: cIndex,
-          items: items,
-          backgroundColor: Color.fromARGB(255, 196, 134, 207),
-          onItemSelect: (value) {
-            setState(() {
-              cIndex = value;
-            });
-          },
-          itemHoverColor: Color.fromARGB(255, 105, 6, 100),
-          itemHoverColorOpacity: .5,
-          activeIconColor: Colors.black,
-          deActiveIconColor: Colors.black38,
-          barRadius: 30,
-          customBorderRadius: BorderRadius.circular(40),
-          textStyle: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-          itemHoverBorderRadius: 30,
-        ),
+        bottomNavigationBar: Cbn(),
       ),
     );
+  }
+}
+
+class WidgetModel {
+  final double width, height;
+  final double xDistance, yDistance;
+
+  WidgetModel({
+    required this.yDistance,
+    required this.height,
+    required this.width,
+    required this.xDistance,
+  });
+
+  @override
+  String toString() {
+    return 'WidgetModel(width: $width, height: $height, xDistance: $xDistance, yDistance: $yDistance)';
+  }
+}
+
+class Cbn extends StatefulWidget {
+  const Cbn({Key? key}) : super(key: key);
+
+  @override
+  _CbnState createState() => _CbnState();
+}
+
+class _CbnState extends State<Cbn> {
+  late List<GlobalKey> iconKeys;
+  bool showFloating = false;
+  int selectedItem = 0;
+  List<WidgetModel> widgetsInfo = [];
+  Orientation? currentOrientation;
+
+  @override
+  void initState() {
+    super.initState();
+    iconKeys = List.generate(6, (_) => GlobalKey());
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      calculateIconPositionsAndSizes();
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (currentOrientation == null)
+      currentOrientation = MediaQuery.of(context).orientation;
+
+    super.didChangeDependencies();
+  }
+
+  void calculateIconPositionsAndSizes() {
+    widgetsInfo.clear();
+    for (var i = 0; i < iconKeys.length; i++) {
+      final RenderBox iconBox =
+          iconKeys[i].currentContext!.findRenderObject() as RenderBox;
+      final Offset iconPosition = iconBox.localToGlobal(Offset.zero);
+
+      final RenderBox widgetBox =
+          iconKeys[i].currentContext!.findRenderObject() as RenderBox;
+      widgetsInfo.add(
+        WidgetModel(
+          height: widgetBox.size.height,
+          width: widgetBox.size.width,
+          xDistance: iconPosition.dx,
+          yDistance: iconPosition.dy,
+        ),
+      );
+    }
+    setState(() {
+      showFloating = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var barBgSize = 80.0;
+    var floatingIconHeight = 50.0;
+    var floatingButtonHorizontalPadding = 40.0;
+
+    return OrientationBuilder(builder: (context, orientation) {
+      print(orientation);
+      print(currentOrientation);
+      if (orientation != currentOrientation) {
+        currentOrientation = orientation;
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          calculateIconPositionsAndSizes();
+        });
+        print(widgetsInfo);
+      }
+
+      return Container(
+        color: Colors.blue,
+        child: Stack(
+          children: [
+            if (showFloating)
+              AnimatedPositioned(
+                duration: Duration(milliseconds: 300),
+                top: (barBgSize - floatingIconHeight) / 2,
+                width: widgetsInfo[selectedItem].width +
+                    floatingButtonHorizontalPadding,
+                height: floatingIconHeight,
+                left: widgetsInfo[selectedItem].xDistance -
+                    (floatingButtonHorizontalPadding / 2),
+                curve: Curves.ease,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+            Container(
+              height: barBgSize,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(
+                    6,
+                    (index) => GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedItem = index;
+                        });
+                      },
+                      child: IconButton(
+                        icon: Icon(
+                          index % 2 == 0 ? Icons.access_alarm : Icons.abc,
+                          size: 25,
+                        ),
+                        key: iconKeys[index],
+                        onPressed: () {
+                          setState(() {
+                            selectedItem = index;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
